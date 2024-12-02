c72221ed4cf5b38ed1eade92f93492d29a3a94c8
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2024-12-02 16:29:02 +0100

    feat: ex 2

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -14,12 +14,11 @@ const fourthTeacher = teachers.at(4);
 
 console.log(fourthTeacher)
 
-
-
-
-
-
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
+
+teachers[5] = `Patrick`;
+
+console.log(teachers)
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
 const lastTeacher = null;
